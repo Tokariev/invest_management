201eb1ac8a264ff686a0e12f1da0bed36e4502eb
Mykola Tokariev <mykola.tokariev@gmail.com>
2021-04-02 10:31:59 +0200

    Fetch list from Coingecko API

--- lib/http/coingecko_api.dart
+++ lib/http/coingecko_api.dart
@@ -1,13 +1,19 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'dart:async';
+import '../model/coin_model.dart';
 
-Uri url = Uri.parse('https://api.coingecko.com/api/v3/coins/list');
+class CoingeckoAPI {
+  Future<List<Coin>> fetchCoins() async {
+    Uri coinsListAPIUrl =
+        Uri.parse('https://api.coingecko.com/api/v3/coins/list');
+    final response = await http.get(coinsListAPIUrl);
 
-class Coingecko {
-  Future<http.Response> getCoinList() async {
-    http.Response response = await http.get(url);
-    print(response.body);
-    return response;
+    if (response.statusCode == 200) {
+      List jsonResponse = json.decode(response.body);
+      return jsonResponse.map((job) => new Coin.fromJson(job)).toList();
+    } else {
+      throw Exception('Failed to load jobs from API');
+    }
   }
 }
